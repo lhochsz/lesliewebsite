67519f7a55deae015dadc36950c804671bbd20bc
Leslie Hochsztein <lhochsz@gmail.com>
2016-08-25 22:36:36 -0400

    Trying to fix parallax for mobile

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
+//nav bar attach to top
 $(document).ready(function() {
 
   $(window).scroll(function () {
-      //if you hard code, then use console
-      //.log to determine when you want the
-      //nav bar to stick.
       console.log($(window).scrollTop())
     if ($(window).scrollTop() > 280) {
       $('#nav_bar').addClass('navbar-fixed');
     }
     if ($(window).scrollTop() < 281) {
       $('#nav_bar').removeClass('navbar-fixed');
+    }
+  });
+});
+
+//scrolling
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
     }
   });
 });
